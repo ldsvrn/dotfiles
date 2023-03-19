e4dd3ee02e62f2d9e8bf74673cf8b0f48e631d53
ldsvrn <louis2555@orange.fr>
2023-03-19 18:03:57 +0100

    updated picom conf, removed useless neovim stuff
    fix vsync

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,3 @@
 require("ld.bootstrap") -- Packer Auto-Installer
 require("ld.base")
 require("ld.plugins")
-
--- Brief help
--- :PackerList       - lists configured plugins
--- :PackerInstall    - installs plugins; append `!` to update or just :PackerUpdate
--- :PackerSearch foo - searches for foo; append `!` to refresh local cache
--- :PackerClean      - confirms removal of unused plugins; append `!` to auto-approve removal
--- see :h packer for more details or wiki for FAQ
-
--- General Settings
-vim.cmd('autocmd vimenter * ++nested filetype plugin indent on')
--- To ignore plugin
-
